285aee223123a99ff2b8f288cbe25e17cf543c77
Lukas656 <le.lu.ka2018@gmail.com>
2024-07-18 23:25:08 -0300

    feat: incluindo novas rotas e novas requisições a api

--- src/controller/addUser.js
+++ src/controller/addUser.js
@@ -16,7 +16,6 @@ async function criarUser(newUser) {
         if (!usuario) {
             usuario = new Usuario(newUser);
             await usuario.save();
-            console.log(`Inserindo ${newUser.owner_name}`);
             return { userID: usuario._id };
         }
 
@@ -27,8 +26,37 @@ async function criarUser(newUser) {
         throw error;
     }
 }
+// Atualizar destino data de inicio e fim da minha viajem
+async function updateTrips(id, newTrips) {
+    try {
+        const usuario = await Usuario.findById(id);
 
+        if (!usuario) {
+            throw new Error('Usuário não encontrado.');
+        }
 
+        // Verifica se a data de início é anterior à data atual
+        const currentDate = new Date();
+        if (new Date(newTrips.starts_at) < currentDate) {
+            throw new Error('A data de início não pode ser anterior à data atual.');
+        }
+
+        // Atualiza os campos
+        usuario.destination = newTrips.destination;
+        usuario.starts_at = newTrips.starts_at;
+        usuario.ends_at = newTrips.ends_at;
+
+        // Zera o array de atividades
+        usuario.activites = [];
+
+        await usuario.save();
+        return usuario;
+
+    } catch (error) {
+        console.error('Erro ao atualizar o usuário:', error);
+        throw error;
+    }
+}
 // Buscar usuario pelo ID
 async function getUserByID(id) {
     try {
@@ -110,8 +138,85 @@ async function createActiviies(id, activity) {
         throw error;
     }
 }
+// Adicionar links 
+async function addLink(userId, newLink) {
+    try {
+        const usuario = await Usuario.findById(userId);
+        if (!usuario) {
+            throw new Error('Usuário não encontrado');
+        }
+        
+        usuario.important_links.push(newLink);
 
+        await usuario.save();
 
+        return usuario.important_links;
+    } catch (error) {
+        console.error('Erro ao adicionar o link:', error.message);
+        throw error;
+    }
+}
+// Deletar Links
+async function deleteLink(userId, linkId) {
+    try {
+        const usuario = await Usuario.findById(userId);
+        if (!usuario) {
+            throw new Error('Usuário não encontrado');
+        }
+        // Encontrar o índice do link a ser removido
+        const linkIndex = usuario.important_links.findIndex(link => link._id.toString() === linkId);
+        if (linkIndex === -1) {
+            throw new Error('Link não encontrado');
+        }
+
+        // Remover o link do array
+        usuario.important_links.splice(linkIndex, 1);
+
+        // Salvar o usuário atualizado
+        await usuario.save();
+
+        return usuario.important_links;
+    } catch (error) {
+        console.error('Erro ao deletar o link:', error.message);
+        throw error;
+    }
+}
+// Deletar Convidados
+async function deleteEmail(userId, emailId) {
+    try {
+        const usuario = await Usuario.findById(userId);
+        if (!usuario) {
+            throw new Error('Usuário não encontrado');
+        }
+
+        usuario.emails_to_invite = usuario.emails_to_invite.filter(userEmail => userEmail !== emailId);
+
+        await usuario.save();
+
+        return usuario.emails_to_invite;
+    } catch (error) {
+        console.error('Erro ao deletar o e-mail:', error.message);
+        throw error;
+    }
+}
+// Adicionar Email de Convidados
+async function addEmail(userId, newEmail) {
+    try {
+        const usuario = await Usuario.findById(userId);
+        if (!usuario) {
+            throw new Error('Usuário não encontrado');
+        }
+
+        usuario.emails_to_invite.push(newEmail);
+
+        await usuario.save();
+
+        return usuario.emails_to_invite;
+    } catch (error) {
+        console.error('Erro ao adicionar o e-mail:', error.message);
+        throw error;
+    }
+}
 
 const dataFunctions = {
     criarUser,
@@ -120,6 +225,12 @@ const dataFunctions = {
     getlinks,
     getActivities,
     createActiviies,
+    updateTrips,
+    deleteLink,
+    addLink,
+    deleteEmail,
+    addEmail,
+
 }
 
 module.exports = dataFunctions;
--- src/routers/router.js
+++ src/routers/router.js
@@ -8,12 +8,28 @@ router.use(bodyParser.json());
 router.use(cors());
 
 
-// Exibe mensagem se esta funcionando a conexao
-router.get('/', async (req, res) => {
-    res.status(200).send({ success: true, message: "Sistema Funcionando!!!" })
+// Criar Viagem 
+router.post('/trips', async (req, res) => {
+    try {
+        const newUser = req.body;
+        const result = await dataFunctions.criarUser(newUser);
+        res.status(201).send(result);
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
 });
-
-// pega destino, data , convidados e nome e emal do adm;
+// Alterar Local e Data;
+router.put('/trips/:id', async (req, res) => {
+    try {
+        let tripID = req.params.id;
+        let newTrips = req.body;
+        let user = await dataFunctions.updateTrips(tripID, newTrips);
+        res.status(200).send(user);
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+});
+// Pesquisa Usuário pelo ID ;
 router.get('/trips/:id', async (req, res) => {
     try {
         let tripID = req.params.id;
@@ -23,7 +39,28 @@ router.get('/trips/:id', async (req, res) => {
         res.status(500).send({ error: error.message });
     }
 });
-// Exibe participantes;
+// Criar Atividades;
+router.post('/trips/:id/activites', async (req, res) => {
+    try {
+        let tripID = req.params.id;
+        let activities = req.body;
+        let user = await dataFunctions.createActiviies(tripID, activities);
+        res.status(200).send(user);
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+});
+// Listar Atividades;
+router.get('/trips/:id/activites', async (req, res) => {
+    try {
+        let tripID = req.params.id;
+        let user = await dataFunctions.getActivities(tripID);
+        res.status(200).send(user);
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+});
+// Exibir Convidados;
 router.get('/trips/:id/gests', async (req, res) => {
     try {
         let tripID = req.params.id;
@@ -43,40 +80,60 @@ router.get('/trips/:id/links', async (req, res) => {
         res.status(500).send({ error: error.message });
     }
 });
-// Exibe Atividades;
-router.get('/trips/:id/activites', async (req, res) => {
+// Adicionar links importantes;
+router.post('/trips/:id/links', async (req, res) => {
     try {
-        let tripID = req.params.id;
-        let user = await dataFunctions.getActivities(tripID);
-        res.status(200).send(user);
+        const tripId  = req.params.id;
+        const newLink = req.body;
+        const updatedLinks = await dataFunctions.addLink(tripId, newLink);
+        res.status(200).json(updatedLinks);
     } catch (error) {
-        res.status(500).send({ error: error.message });
+        res.status(500).json({ error: error.message });
     }
 });
-// Criar Atividades;
-router.post('/trips/:id/activites', async (req, res) => {
+// Deletar links importantes;
+router.delete('/trips/:id/links/:linkId', async (req, res) => {
     try {
-        let tripID = req.params.id;
-        let activities = req.body;
-        let user = await dataFunctions.createActiviies(tripID, activities);
-        res.status(200).send(user);
+        const tripId  = req.params.id;
+        const linkId  = req.params.linkId;
+        const updatedLinks = await dataFunctions.deleteLink(tripId, linkId);
+        res.status(200).json(updatedLinks);
     } catch (error) {
-        res.status(500).send({ error: error.message });
+        res.status(500).json({ error: error.message });
     }
 });
-
-// pega destino, data , convidados e nome e emal do adm;
-router.post('/trips', async (req, res) => {
+// Deletar Email de Convidados;
+router.delete('/trips/:id/links/:linkId', async (req, res) => {
     try {
-        const newUser = req.body;
-        const result = await dataFunctions.criarUser(newUser);
-        res.status(201).send(result);
+        const { tripId, linkId } = req.params;
+        const updatedLinks = await dataFunctions.deleteLink(tripId, linkId);
+        res.status(200).json(updatedLinks);
     } catch (error) {
-        res.status(400).send({ error: error.message });
+        res.status(500).json({ error: error.message });
     }
 });
-
-
+// Rota para adicionar um e-mail
+router.post('/trips/:id/gests', async (req, res) => {
+    try {
+        const tripId  = req.params.id;
+        const newEmail = req.body.email;
+        const updatedEmails = await dataFunctions.addEmail(tripId, newEmail);
+        res.status(200).json(updatedEmails);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+// Rota para deletar um e-mail
+router.delete('/trips/:id/gests/:emailId', async (req, res) => {
+    try {
+        const tripId = req.params.id;
+        const emailId  = req.params.emailId;
+        const updatedEmails = await dataFunctions.deleteEmail(tripId, emailId);
+        res.status(200).json(updatedEmails);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
 
 
 module.exports = router;
